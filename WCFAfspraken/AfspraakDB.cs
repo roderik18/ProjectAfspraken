b8f1ab892ac8da691f8e546be17ffeb50db1a992
Roderik Masure <roderikmasure@gmail.com>
2015-04-22 14:41:25 +0200

    Dit is een test bla bla

---
+++ WCFAfspraken/AfspraakDB.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WCFAfspraken
+{
+    public class AfspraakDB  
+    {
+        public AfspraakDB()
+        {
+
+        }
+
+        public List<Afspraak> GetAfspraken()
+        {
+            List<Afspraak> list = new List<Afspraak>();
+            string sql = "SELECT Afspraken.*, Cursisten.*, Trajectbegeleiders.* FROM Afspraken " +
+                        "INNER JOIN Cursisten ON Afspraken.CursistId=Cursisten.CursistId " +
+                        "INNER JOIN Trajectbegeleiders ON Afspraken.TBid=Trajectbegeleiders.TBid";
+
+            using (SqlConnection con = new SqlConnection(GetConnectionString()))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    con.Open();
+                    SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    while (dr.Read())
+                    {
+                        Afspraak a = new Afspraak();
+                        Cursist c = new Cursist();
+                        TrajectBegeleider t = new TrajectBegeleider();
+
+                        t.TBid = dr["TBid"].ToString();
+                        t.Naam = dr[13].ToString();
+                        t.Voornaam = dr[14].ToString();
+
+                        c.CursistId = dr["CursistId"].ToString();
+                        c.Naam = dr[9].ToString();
+                        c.Voornaam = dr[10].ToString();
+
+                        a.Id = Convert.ToInt32(dr[0]);
+                        a.StartUur = Convert.ToDateTime(dr["StartUur"]);
+                        a.StopUur = Convert.ToDateTime(dr["StopUur"]);
+                        a.Comments = dr["Comments"].ToString();
+                        a.Vastgelegd = Convert.ToBoolean(dr["Vastgelegd"]);
+                        a.cursist = c;
+                        a.TB = t;
+
+                        list.Add(a);
+                    }
+                }
+            }
+            return list;
+        }
+
+         private string GetConnectionString()
+        {
+            return "Data Source=RODERIK-PC\\SQLEXPRESS;Initial Catalog=CursistAfspraken;Integrated Security=True";
+        }
+    }
+}
---
+++ WCFAfspraken/IService1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFAfspraken
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    
+    [ServiceContract]
+    public interface IAfspraakService
+    {
+
+        [OperationContract]
+        List<Afspraak> GetAfspraken();
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Afspraak
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public Cursist cursist { get; set; }
+
+        [DataMember]
+        public TrajectBegeleider TB { get; set; }
+
+        [DataMember]
+        public DateTime StartUur { get; set; }
+
+        [DataMember]
+        public DateTime StopUur { get; set; }
+
+        [DataMember]
+        public string Comments { get; set; }
+
+        [DataMember]
+        public Boolean Vastgelegd { get; set; }
+
+        public void VastLeggen()
+        {
+            if (!this.Vastgelegd)
+            {
+                this.Vastgelegd = true;
+            }
+        }
+
+        public void Cancellen()
+        {
+            if (this.Vastgelegd)
+            {
+                this.Vastgelegd = false;
+                //Stuur bericht naar afzender (cursist) dat afspraak gecancelled is.
+                //comment van TB met rede
+            }
+        }
+
+        public void NietVastleggen()
+        { //stuur bericht naar cursist dat afspraak niet is vastgelegd.
+            //comment van TB met rede
+        }
+    }
+
+    [DataContract]
+    public class Cursist
+    {
+        [DataMember]
+        public string CursistId { get; set; }
+
+        [DataMember]
+        public string Naam { get; set; }
+
+        [DataMember]
+        public string Voornaam { get; set; }
+
+        public void NieuweAfspraak()
+        {
+            //nieuwe afspraak maken.
+        }
+    }
+
+    [DataContract]
+    public class TrajectBegeleider
+    {
+        [DataMember]
+        public string TBid { get; set; }
+
+        [DataMember]
+        public string Naam { get; set; }
+
+        [DataMember]
+        public string Voornaam { get; set; }
+    }
+}
